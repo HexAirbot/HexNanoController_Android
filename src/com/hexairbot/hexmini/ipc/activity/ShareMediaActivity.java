56a1adcce411f55d4ab8619164ce0a6935b9e39e
xialinchong <476454979@qq.com>
2014-08-20 18:15:07 +0800

    add share function

--- src/com/hexairbot/hexmini/ipc/activity/ShareMediaActivity.java
+++ src/com/hexairbot/hexmini/ipc/activity/ShareMediaActivity.java
@@ -2,24 +2,28 @@ package com.hexairbot.hexmini.ipc.activity;
 
 import java.io.File;
 
+import com.hexairbot.hexmini.FeedbackActivity;
 import com.hexairbot.hexmini.R;
-import com.vmc.ipc.util.MediaUtil;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.ImageView;
 
 public class ShareMediaActivity extends Activity {
 
-	private Button sharebtn = null;
+	private String media_path = null;
+	private String media_type = null;
 	private boolean actionInitFirst = false;
 	private ImageView selectimg = null;
 
@@ -29,39 +33,62 @@ public class ShareMediaActivity extends Activity {
 		setContentView(R.layout.share_media);
 		initActionBar();
 		Intent intent = getIntent();
-		final String media_path = intent.getStringExtra("media_path");
-		final String media_type = intent.getStringExtra("media_type");
-		
-		sharebtn = (Button)findViewById(R.id.sharebtn);
-		sharebtn.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				SharePhoto(media_path, media_type, ShareMediaActivity.this);
-			}
-		});
-		
-		selectimg = (ImageView)findViewById(R.id.selectimg);
+		media_path = intent.getStringExtra("media_path");
+		media_type = intent.getStringExtra("media_type");
+
+		selectimg = (ImageView) findViewById(R.id.selectimg);
 		File file = new File(media_path);
-		if(file.exists()){
+		if (file.exists()) {
 			Bitmap bm = BitmapFactory.decodeFile(media_path);
 			selectimg.setImageBitmap(bm);
 		}
-	} 
-	
-	public void SharePhoto(String mediaUri,String type,final Activity activity) { 
-	    Intent shareIntent = new Intent(Intent.ACTION_SEND); 
-	    File file = new File(mediaUri);
-	    shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file));
-		shareIntent.setType(type);
-	    
-	    startActivity(Intent.createChooser(shareIntent, activity.getTitle())); 
 	}
-	
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.sharemenu, menu);
+		return true;
+
+	}
+
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.sharebtn:
+			SharePhoto(media_path, media_type, ShareMediaActivity.this);
+			break;
+		case android.R.id.home:
+			ShareMediaActivity.this.finish();
+			break;
+		case R.id.delbtn:
+			new AlertDialog.Builder(ShareMediaActivity.this)
+				.setTitle(R.string.delbtnname)
+//				.setIcon(android.R.drawable.ic_dialog_alert)
+				.setMessage(R.string.del_text)
+				.setPositiveButton(R.string.dialog_ok, new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int whichButton) {
+						File file = new File(media_path);
+						deleteFile(file);
+						dialog.dismiss();
+						ShareMediaActivity.this.finish();
+					}
+				})
+				.setNegativeButton(R.string.dialog_cancel, new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int whichButton) {
+						dialog.dismiss();
+					}
+				}).show();
+			break;
+		default:
+			break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
 	private void initActionBar() {
 		ActionBar actionBar = this.getActionBar();
 		actionBar.setBackgroundDrawable(this.getResources().getDrawable(
-				R.drawable.barre_haut));
+				R.drawable.bar_top));
 		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		if (!actionInitFirst) {
@@ -71,8 +98,30 @@ public class ShareMediaActivity extends Activity {
 			int change = actionBar.getDisplayOptions() ^ flags;
 			actionBar.setDisplayOptions(change, flags);
 		}
-//		actionBar.setTitle(R.string.gallery_photo_title);
-
+		actionBar.setTitle("");
 	}
-	
+
+	public void SharePhoto(String mediaUri, String type, final Activity activity) {
+		Intent shareIntent = new Intent(Intent.ACTION_SEND);
+		File file = new File(mediaUri);
+		shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file));
+		shareIntent.setType(type);
+
+		startActivity(Intent.createChooser(shareIntent, activity.getTitle()));
+	}
+
+	public void deleteFile(File file) {
+		if (file.exists()) {
+			if (file.isFile()) {
+				file.delete();
+			} else if (file.isDirectory()) {
+				File files[] = file.listFiles();
+				for (int i = 0; i < files.length; i++) {
+					deleteFile(files[i]);
+				}
+			}
+			file.delete();
+		}
+	}
+
 }
